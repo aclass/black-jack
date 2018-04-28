9d82b5d9244728d8e1d20774f42ce3956978f47b
吉谷優介 <Yusuke@yoshitaniyuusuke-no-Mac-mini.local>
2018-04-29 00:17:15 +0900

    カード抽選

--- app.rb
+++ app.rb
@@ -1,19 +1,21 @@
-# 数字
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-# マーク
-suits = ["S", "H", "C", "D"]
-
 # 抽選済みカード
-already = []
+@already = []
 
-# カード
-# card = suits[rand(4)] + num[rand(13)].to_s
+# ユーザカード
+@user = []
+# ディーラーカード
+@dealer = []
 
-5.times do
+# カード抽選
+def pickup_card()
+  num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  suits = ["S", "H", "C", "D"]
   card = suits[rand(4)] + num[rand(13)].to_s
-  unless already.include?(card)
-    puts card
-    already << card
+  # カードチェック
+  if @already.include?(card)
+    pickup_card()
   end
+  return card
 end
+
+@already << pickup_card()
