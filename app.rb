9d9b8e9138403daf593d6a1467cd0ff6dbd26eb1
吉谷優介 <Yusuke@yoshitaniyuusuke-no-Mac-mini.local>
2018-04-29 01:49:41 +0900

    勝敗判定

--- app.rb
+++ app.rb
@@ -11,6 +11,9 @@ def pickup_card()
   num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
   suits = ["S", "H", "C", "D"]
   n = num[rand(13)]
+  if n > 10
+    n = 10
+  end
   s = suits[rand(4)]
   card = s + n.to_s
   result = {
@@ -21,11 +24,30 @@ def pickup_card()
   if @already.include?(result[:card])
     pickup_card()
   end
+  @already << card
   return result
 end
 
-def check_num()
+# バースト判定
+def is_over(nums)
+  result = false
+  if nums.inject(:+) > 21
+    result = true
+  end
+  return result
+end
 
+# 勝敗判定
+def check_winner
+  result = "user"
+  user_num = @user.inject(:+)
+  dealer_num = @dealer.inject(:+)
+  if user_num < dealer_num
+    result = "dealer"
+  elsif user_num == dealer_num
+    result = "draw"
+  end
+  return result
 end
 
 puts "start game!"
@@ -47,4 +69,36 @@ count = 1
   count += 1
 end
 
-@already << pickup_card()
+puts "あなたのカードの合計：#{@user.inject(:+)}"
+puts "------------------------"
+puts "カードを引きますか？ Y/N"
+puts "------------------------"
+
+input = gets.chomp
+while input == "Y"
+  card = pickup_card()
+  @already << card[:card]
+  @user << card[:num]
+  puts "あなたの引いたカードは#{card[:card]}です"
+  if is_over(@user)
+    break;
+  end
+  puts "あなたのカードの合計：#{@user.inject(:+)}"
+  puts "------------------------"
+  puts "カードを引きますか？ Y/N"
+  puts "------------------------"
+  input = gets.chomp
+end
+
+# 勝敗判定
+puts "ディーラーの2枚目のカード：#{@already[3]}"
+puts "あなた：#{@user.inject(:+)}"
+puts "ディーラー：#{@dealer.inject(:+)}"
+case check_winner()
+when "user"
+  puts "あなたの勝ちです"
+when "dealer"
+  puts "ディーラーの勝ちです"
+else
+  puts "引き分けです"
+end
